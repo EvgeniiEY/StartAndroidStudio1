cebc0f3a069503a766a86034de691957d786caaa
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-09-07 21:54:38 +0300

    task 1.2.3 question

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,11 +1,25 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list_item)
     }
+
+
+
+
+//    fun showPopup(v: View?) {
+//        val popup = PopupMenu(this, v)
+//        val inflater = popup.menuInflater
+//        inflater.inflate(R.menu.actions, popup.menu)
+//        popup.show()
+//    }
 }
