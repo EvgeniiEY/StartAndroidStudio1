afbdf108173fe9de1415cdf5c5b7024c72393802
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-09-21 15:37:25 +0300

    task 1.2.4 question

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,17 +3,23 @@ package ru.netology.nmedia
 import android.R
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import android.widget.ListView
 import android.widget.PopupMenu
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_item)
+        val listView = findViewById<ImageButton>(R.id.)
     }
 
-
+@Nullable
+public final <T extends View> T findViewById
 
 
 //    fun showPopup(v: View?) {
