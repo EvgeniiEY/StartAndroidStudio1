e542b1400d781e7a3216001bd4fa66962106743e
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-09-23 14:19:39 +0300

    task 1.2.4.1

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,25 +1,64 @@
 package ru.netology.nmedia
 
-import android.R
+
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
-import android.widget.ListView
-import android.widget.PopupMenu
-import androidx.annotation.Nullable
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+import java.text.DecimalFormat
+
+
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        var likeStatus = 0
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_item)
-        val listView = findViewById<ImageButton>(R.id.)
+        setContentView(R.layout.activity_main)
+        likesButton.setOnClickListener {
+            if (likeStatus == 1) {
+                likesButton.setImageResource(R.drawable.ic_baseline_favorite_24)
+                likeStatus = 0
+                val countStringLikes = likesNumber.text.toString()
+                var counterL: Int = Integer.parseInt(countStringLikes)
+                counterL--
+                if(counterL <= 1000) {
+                    likesNumber.text = counterL.toString()
+                } else {likesNumber.text = "1K"}
+
+            } else {
+                likesButton.setImageResource(R.drawable.ic_liked_24)
+                likeStatus = 1
+                val countStringLikes = likesNumber.text.toString()
+                var counterL: Int = Integer.parseInt(countStringLikes)
+                counterL++
+                likesNumber.text = counterL.toString()
+
+            }
+        }
     }
 
-@Nullable
-public final <T extends View> T findViewById
+
+    fun countReposts(view: View) {
+        val countStringReposts = repostsNumber.text.toString()
+        var counterR: Int = Integer.parseInt(countStringReposts)
+        counterR++
+        repostsNumber.text = counterR.toString()
+    }
+}
+
+
+
+
+//@Nullable
+//public final <T extends View> T findViewById(@
 
 
 //    fun showPopup(v: View?) {
@@ -28,4 +67,4 @@ public final <T extends View> T findViewById
 //        inflater.inflate(R.menu.actions, popup.menu)
 //        popup.show()
 //    }
-}
+//}
