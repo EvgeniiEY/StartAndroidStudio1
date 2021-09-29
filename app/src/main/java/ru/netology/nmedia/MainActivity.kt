e82560be3ff8920e5c01561bd5e5dc7fe67e8ecf
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-09-29 15:41:47 +0300

    task 1.2.4.1 1000 to 1K reserve commit2

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,8 +1,8 @@
 package ru.netology.nmedia
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -12,17 +12,19 @@ import ru.netology.nmedia.databinding.ActivityMainBinding
 
 
 import java.text.DecimalFormat
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
 
 
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         var likeStatus = 0
         val countStringLikes = binding.likesNumber.text.toString()
-        var counterL: Int = Integer.parseInt(countStringLikes)
+        var counterL: Double = countStringLikes.toDouble()
         setContentView(binding.root)
         binding.likesButton.setOnClickListener {
 
@@ -31,43 +33,58 @@ class MainActivity : AppCompatActivity() {
                 binding.likesButton.setImageResource(R.drawable.ic_liked_24)
                 likeStatus = 1
                 counterL++
-                if (counterL < 1000) {
-                    binding.likesNumber.text = counterL.toString()
+
+                if (Math.abs(counterL / 1_000) >= 1) {
+//                    val doubleRaw =
+                    binding.likesNumber.text = getString(R.string.app_likes, (counterL / 1_000).toString(), "k")
 
 
-                } else if(counterL in 1000..1099){
-                    binding.likesNumber.text = "1K"
+                } else if (Math.abs(counterL / 1_000_000) >= 1) {
+                    binding.likesNumber.text = getString(R.string.app_likes, (counterL / 1_000_000).toString(), "m")
+                } else {
+                    binding.likesNumber.text = getString(R.string.app_likes, counterL.toString(), "")
+
+                }
 
 
-            } else {
-                if (likeStatus == 1) {
-                    binding.likesButton.setImageResource(R.drawable.ic_baseline_favorite_24)
-                    likeStatus = 0
+
+        } else {
+            if (likeStatus == 1) {
+                binding.likesButton.setImageResource(R.drawable.ic_baseline_favorite_24)
+                likeStatus = 0
 //                    val countStringLikes = likesNumber.text.toString()
 //                    var counterL: Int = Integer.parseInt(countStringLikes)
-                    counterL--
-                    binding.likesNumber.text = counterL.toString()
-                    if (counterL < 1000) {
+                counterL--
+                when {
+                    abs(counterL / 1000000) > 1 -> {
+                        binding.likesNumber.text = (counterL / 1000000).toString()
+                        getString(R.string.app_likes, binding.likesNumber.text, "k")
+                    }
+                    abs(counterL / 1000) > 1 -> {
+                        binding.likesNumber.text = (counterL / 1000).toString()
+                        getString(R.string.app_likes, binding.likesNumber.text, "k")
+                    }
+                    else -> {
                         binding.likesNumber.text = counterL.toString()
-                    } else {
-                        binding.likesNumber.text = "1K"
                     }
                 }
             }
         }
-
-        binding.repostsButton.setOnClickListener {
-
-
-
-                val countStringReposts = binding.repostsNumber.text.toString()
-                var counterR: Int = Integer.parseInt(countStringReposts)
-                counterR++
-                binding.repostsNumber.text = counterR.toString()
-
-
-        }
     }
+
+    binding.repostsButton.setOnClickListener {
+
+
+        val countStringReposts = binding.repostsNumber.text.toString()
+        var counterR: Int = Integer.parseInt(countStringReposts)
+        counterR++
+        binding.repostsNumber.text = counterR.toString()
+
+
+    }
+}
+
+
 }
 
 //android:onClick= "countReposts"
