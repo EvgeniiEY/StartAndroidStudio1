412b0c4be51b3bd38562d0eccb99b8b0dc1262f5
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-10-01 15:53:50 +0300

    task 1.2.4.1 1000 to 1K reserve commit4

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,17 +3,10 @@ package ru.netology.nmedia
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import java.math.RoundingMode
-
-
-import java.text.DecimalFormat
-import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,74 +22,94 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.likesButton.setOnClickListener {
 
-        // ВЕТКА ВЫГРУЖЕНА
+
+
             if (likeStatus == 0) {
                 binding.likesButton.setImageResource(R.drawable.ic_liked_24)
                 likeStatus = 1
                 counterL++
 
-                if (Math.abs(counterL / 1_000) >= 1) {
-                    val counterLrounded = (counterL/1000).toBigDecimal().setScale(1, RoundingMode.FLOOR).toDouble()
-                    binding.likesNumber.text = getString(R.string.app_likes, (counterLrounded).toString(), "k")
+                if (Math.abs(counterL / 1_000) >= 1 && Math.abs(counterL / 1_000) < 1000) {
+                    val counterLrounded =
+                        (counterL / 1_000).toBigDecimal().setScale(1, RoundingMode.FLOOR).toDouble()
+                    binding.likesNumber.text =
+                        getString(R.string.app_likes, (counterLrounded).toString(), "k")
+
 
                 } else if (Math.abs(counterL / 1_000_000) >= 1) {
-                    binding.likesNumber.text = getString(R.string.app_likes, (counterL / 1_000_000).toString(), "m")
+                    val counterLrounded =
+                        (counterL / 1_000_000).toBigDecimal().setScale(1, RoundingMode.UNNECESSARY)
+                            .toDouble()
+                    binding.likesNumber.text =
+                        getString(R.string.app_likes, (counterLrounded).toString(), "m")
                 } else {
-                    binding.likesNumber.text = getString(R.string.app_likes, counterL.toString(), "")
+                    val countStringLikes = binding.likesNumber.text.toString()
+                    var counterL: Int = Integer.parseInt(countStringLikes)
+
+                    binding.likesNumber.text = counterL.toString()
+
 
                 }
 
+            } else {
+                if (likeStatus == 1) {
+                    binding.likesButton.setImageResource(R.drawable.ic_baseline_favorite_24)
+                    likeStatus = 0
+                    counterL--
+                    if (Math.abs(counterL / 1_000) >= 1 && Math.abs(counterL / 1_000) < 1000) {
+                        counterL--
+                        val counterLrounded =
+                            (counterL / 1_000).toBigDecimal().setScale(0, RoundingMode.HALF_DOWN)
+                                .toDouble()
+                        binding.likesNumber.text =
+                            getString(R.string.app_likes, (counterLrounded).toString(), "k")
 
+                    } else if (Math.abs(counterL / 1_000_000) >= 1) {
+                        val counterLrounded =
+                            (counterL / 1_000_000).toBigDecimal().setScale(1, RoundingMode.FLOOR)
+                                .toDouble()
+                        binding.likesNumber.text =
+                            getString(R.string.app_likes, (counterLrounded).toString(), "m")
+                    } else {
+                        val countStringLikes = binding.likesNumber.text.toString()
+                        var counterL: Int = Integer.parseInt(countStringLikes)
 
-        } else {
-            if (likeStatus == 1) {
-                binding.likesButton.setImageResource(R.drawable.ic_baseline_favorite_24)
-                likeStatus = 0
-//                    val countStringLikes = likesNumber.text.toString()
-//                    var counterL: Int = Integer.parseInt(countStringLikes)
-                counterL--
-                when {
-                    abs(counterL / 1000000) > 1 -> {
-                        binding.likesNumber.text = (counterL / 1000000).toString()
-                        getString(R.string.app_likes, binding.likesNumber.text, "k")
-                    }
-                    abs(counterL / 1000) > 1 -> {
-                        binding.likesNumber.text = (counterL / 1000).toString()
-                        getString(R.string.app_likes, binding.likesNumber.text, "k")
-                    }
-                    else -> {
                         binding.likesNumber.text = counterL.toString()
                     }
                 }
             }
         }
+
+        binding.repostsButton.setOnClickListener {
+
+
+            val countStringReposts = binding.repostsNumber.text.toString()
+            var counterR: Int = Integer.parseInt(countStringReposts)
+            counterR++
+            binding.repostsNumber.text = counterR.toString()
+
+
+        }
     }
 
-    binding.repostsButton.setOnClickListener {
 
+}
 
-        val countStringReposts = binding.repostsNumber.text.toString()
-        var counterR: Int = Integer.parseInt(countStringReposts)
-        counterR++
-        binding.repostsNumber.text = counterR.toString()
+fun reductionInNumbers(count: Int): String {
+    val formatCount = when {
+        count in 1000..9999 -> {
+            String.format("%.1fK", count / 1000.0)
+        }
+        count in 10000..999999 -> {
+            String.format("%dK", count / 1000)
+        }
+        count > 1000000 -> {
+            String.format("%.1fM", count / 1000000.0)
+        }
 
-
+        else -> {
+            count.toString()
+        }
     }
+    return formatCount
 }
-
-
-}
-
-//android:onClick= "countReposts"
-
-//@Nullable
-//public final <T extends View> T findViewById(@
-
-
-//    fun showPopup(v: View?) {
-//        val popup = PopupMenu(this, v)
-//        val inflater = popup.menuInflater
-//        inflater.inflate(R.menu.actions, popup.menu)
-//        popup.show()
-//    }
-//}
