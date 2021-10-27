fe16f4bdd0c5af0c182677098c4b15dab2a05d69
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-10-27 13:39:07 +0300

    task 2.2. done after remarks

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -49,52 +49,6 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-//        var likeStatus = 0
-//        val countStringLikes = binding.likesNumber.text.toString()
-//        var counterL: Int = Integer.parseInt(countStringLikes)
-//        binding.likesButton.setOnClickListener {
-//
-//            // ВЕТКА ВЫГРУЖЕНА
-//            if (likeStatus == 0) {
-//                binding.likesButton.setImageResource(R.drawable.ic_liked_24)
-//                likeStatus = 1
-//                counterL++
-//                binding.likesNumber.text = reductionInNumbers(counterL)
-//          } else {
-//                if (likeStatus == 1) {
-//                    binding.likesButton.setImageResource(R.drawable.ic_baseline_favorite_24)
-//                    likeStatus = 0
-//                    counterL--
-//                    binding.likesNumber.text = reductionInNumbers(counterL)
-//                }
-//            }
-//        }
-
-//        binding.repostsButton.setOnClickListener {
-//
-//
-//            val countStringReposts = binding.repostsNumber.text.toString()
-//            var counterR: Int = Integer.parseInt(countStringReposts)
-//            counterR++
-//            binding.repostsNumber.text = counterR.toString()
-//
-//
-//        }
-//        binding.likesButton.setOnClickListener {
-//
-//        }
-//        binding.avatar.setOnClickListener {
-//
-//        }
-//        binding.root.setOnClickListener {
-//            println("сработал binding root")
-//        }
-//        binding.threeDotButton.setOnClickListener {
-//
-//        }
-//сработал клик на кнопке лайк
-
-
 fun reductionInNumbers(count: Int): String {
     val formatCount = when {
         count in 1000..9999 -> {
