ee1003517b6a9c7f53b909bcdbd81d78f845ff62
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2022-04-18 16:43:19 +0300

    task 4.3 question JsonSyntaxError

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -1,5 +1,6 @@
 package ru.netology.nmedia.service
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
@@ -10,12 +11,19 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import ru.netology.nmedia.R
+import java.lang.Exception
 import kotlin.random.Random
 
 
 class FCMService : FirebaseMessagingService() {
 
-    private val channelId = "netology"
+    private val channelId = "Netology"
+    private val action = "action"
+    private val content = "content"
+    private val likeChannelId = "like"
+    private val newPostChannelId = "newPost"
+    private val pushTokenChannelId = "pushToken"
+    private val gson = Gson()
 
     override fun onCreate() {
         super.onCreate()
@@ -34,23 +42,79 @@ class FCMService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        println(Gson().toJson(remoteMessage))
-        remoteMessage.data["action"]?.let {
-            when (Action.valueOf(it)) {
-                Action.LIKE -> handleLike(
-                    Gson().fromJson(
-                        remoteMessage.data["content"],
+//        println(Gson().toJson(remoteMessage))
+
+        remoteMessage.data[action]?.let {
+            when (it) {
+                likeChannelId -> handleLike(
+                    gson.fromJson(
+                        remoteMessage.data[content],
                         Like::class.java
+                    )
+                )
+                newPostChannelId -> handlerNewPost(
+                    gson.fromJson(
+                        remoteMessage.data[content],
+                        NewPost::class.java
                     )
                 )
             }
         }
+
+        try {
+            remoteMessage.data["action"]?.let {
+                when (Action.valueOf(it)) {
+
+
+                    Action.LIKE -> handleLike(
+
+                        Gson().fromJson(
+                            remoteMessage.data["content"],
+                            Like::class.java
+                        )
+                    )
+                    Action.NEWPOST -> handlerNewPost(
+                        Gson().fromJson(
+                            remoteMessage.data["content"],
+                            NewPost::class.java
+                        )
+                    )
+                }
+
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+
     }
 
 
     override fun onNewToken(token: String) {
         println(token)
     }
+
+    private fun handlerNewPost(content: NewPost) {
+        val notification = NotificationCompat.Builder(this, newPostChannelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_new_post,
+                    content.userName
+                )
+            )
+            .setContentText(content.postContent)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(content.postContent)
+                    .setSummaryText(content.postContent)
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(this).notify(Random.nextInt(100_000), notification)
+    }
+
 
     private fun handleLike(like: Like) {
         val notification = NotificationCompat.Builder(this, channelId)
@@ -62,20 +126,32 @@ class FCMService : FirebaseMessagingService() {
                     like.postAuthor
                 )
             )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .build()
 
-        NotificationManagerCompat.from(this).notify(Random.nextInt(100_000), notification)
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
     }
+
+
 }
 
 
-    enum class Action {
-        LIKE
-    }
+enum class Action {
+    LIKE,
+    NEWPOST
+}
 
-    data class Like(
-        val userId: Int,
-        val userName: String,
-        val postId: Int,
-        val postAuthor: String
-    )
+data class Like(
+    val userId: Int,
+    val userName: String,
+    val postId: Int,
+    val postAuthor: String
+)
+
+data class NewPost(
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postContent: String
+)
